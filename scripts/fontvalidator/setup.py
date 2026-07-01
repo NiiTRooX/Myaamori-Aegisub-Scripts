d40bc76c257fe42b783e76c023a8a81f51c4743f
NiTRoX <103492540+NiiTRooX@users.noreply.github.com>
2026-07-01 23:21:44 +0200

    version bump

--- scripts/fontvalidator/setup.py
+++ scripts/fontvalidator/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fontvalidator',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=['fontvalidator'],
     install_requires=['ass', 'fonttools', 'ebmlite'],
     entry_points={
